f3609a6861f630fad37a452d3a53f5bee76cae06
sourabhrus <sourabh@wander.today>
2022-10-07 21:51:53 +0530

    Test success in all sceneraio

--- android/src/main/kotlin/today/wander/acs/wander_i_pay88/IPayChannelDelegate.kt
+++ android/src/main/kotlin/today/wander/acs/wander_i_pay88/IPayChannelDelegate.kt
@@ -24,17 +24,18 @@ class IPayChannelDelegate(@Transient private var channel: MethodChannel)
         p5: String?,
         p6: String?
     ) {
-        Handler(Looper.getMainLooper()).post {
-            methodChannel?.invokeMethod(
-                "onPaymentFailed",
-                mapOf(
-                    "transId" to transId,
-                    "refNo" to refNo,
-                    "amount" to amount,
-                    "errDesc" to "",
-                )
-            )
-        }
+        sendSuccessResponse()
+//        Handler(Looper.getMainLooper()).post {
+//            methodChannel?.invokeMethod(
+//                "onPaymentFailed",
+//                mapOf(
+//                    "transId" to transId,
+//                    "refNo" to refNo,
+//                    "amount" to amount,
+//                    "errDesc" to "",
+//                )
+//            )
+//        }
     }
 
     override fun onPaymentSucceeded(transId: String?,
@@ -60,28 +61,44 @@ class IPayChannelDelegate(@Transient private var channel: MethodChannel)
         }
     }
 
+    fun sendSuccessResponse(){
+        Handler(Looper.getMainLooper()).post {
+            channel?.invokeMethod(
+                "onPaymentSucceeded",
+                mapOf(
+                    "transId" to "TI112292763637",
+                    "refNo" to "ZNMA292763637",
+                    "amount" to "1.00",
+                    "remark" to null,
+                    "authCode" to "918176564748903",
+                )
+            )
+        }
+    }
+
     override fun onPaymentFailed(transId: String?,
                                  refNo: String?,
                                  amount: String?,
                                  remark: String?,
                                  errDesc: String?) {
-        print("onPaymentFailed started $errDesc");
-        try {
-            Handler(Looper.getMainLooper()).post {
-                methodChannel?.invokeMethod(
-                    "onPaymentFailed",
-                    mapOf(
-                        "transId" to transId,
-                        "refNo" to refNo,
-                        "amount" to amount,
-                        "remark" to remark,
-                        "errDesc" to errDesc,
-                    )
-                )
-            }
-        } catch (e: Exception) {
-            print("onPaymentCanceled android $e");
-        }
+        sendSuccessResponse()
+//        print("onPaymentFailed started $errDesc");
+//        try {
+//            Handler(Looper.getMainLooper()).post {
+//                methodChannel?.invokeMethod(
+//                    "onPaymentFailed",
+//                    mapOf(
+//                        "transId" to transId,
+//                        "refNo" to refNo,
+//                        "amount" to amount,
+//                        "remark" to remark,
+//                        "errDesc" to errDesc,
+//                    )
+//                )
+//            }
+//        } catch (e: Exception) {
+//            print("onPaymentCanceled android $e");
+//        }
     }
 
     override fun onPaymentCanceled(transId: String?,
@@ -89,57 +106,60 @@ class IPayChannelDelegate(@Transient private var channel: MethodChannel)
                                    amount: String?,
                                    remark: String?,
                                    errDesc: String?) {
-        print("onPaymentCanceled started");
-        try {
-            Handler(Looper.getMainLooper()).post {
-                methodChannel?.invokeMethod(
-                    "onPaymentCanceled",
-                    mapOf(
-                        "transId" to transId,
-                        "refNo" to refNo,
-                        "amount" to amount,
-                        "remark" to remark,
-                        "errDesc" to errDesc,
-                    )
-                )
-            }
-        } catch (e: Exception) {
-            print("onPaymentCanceled android $e");
-        }
+        sendSuccessResponse()
+//        print("onPaymentCanceled started");
+//        try {
+//            Handler(Looper.getMainLooper()).post {
+//                methodChannel?.invokeMethod(
+//                    "onPaymentCanceled",
+//                    mapOf(
+//                        "transId" to transId,
+//                        "refNo" to refNo,
+//                        "amount" to amount,
+//                        "remark" to remark,
+//                        "errDesc" to errDesc,
+//                    )
+//                )
+//            }
+//        } catch (e: Exception) {
+//            print("onPaymentCanceled android $e");
+//        }
     }
 
     fun onBackPressed() {
-        Handler(Looper.getMainLooper()).post {
-            methodChannel?.invokeMethod(
-                "onPaymentCanceled",
-                mapOf(
-                    "transId" to "",
-                    "refNo" to "",
-                    "amount" to "",
-                    "errDesc" to "",
-                )
-            )
-        }
+        sendSuccessResponse()
+//        Handler(Looper.getMainLooper()).post {
+//            methodChannel?.invokeMethod(
+//                "onPaymentCanceled",
+//                mapOf(
+//                    "transId" to "",
+//                    "refNo" to "",
+//                    "amount" to "",
+//                    "errDesc" to "",
+//                )
+//            )
+//        }
     }
 
     override fun onRequeryResult(merchantCode: String?,
                                  refNo: String?,
                                  amount: String?,
                                  result: String?) {
-        try {
-            Handler(Looper.getMainLooper()).post {
-                channel?.invokeMethod(
-                    "onRequeryResult",
-                    mapOf(
-                        "merchantCode" to merchantCode,
-                        "refNo" to refNo,
-                        "amount" to amount,
-                        "result" to result,
-                    )
-                )
-            }
-        } catch (e: Exception) {
-            print("onPaymentCanceled android $e");
-        }
+        sendSuccessResponse()
+//        try {
+//            Handler(Looper.getMainLooper()).post {
+//                channel?.invokeMethod(
+//                    "onRequeryResult",
+//                    mapOf(
+//                        "merchantCode" to merchantCode,
+//                        "refNo" to refNo,
+//                        "amount" to amount,
+//                        "result" to result,
+//                    )
+//                )
+//            }
+//        } catch (e: Exception) {
+//            print("onPaymentCanceled android $e");
+//        }
     }
 }
